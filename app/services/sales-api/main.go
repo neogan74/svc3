61f744a19ff94f337e6f2526f16b3dfd3c6cea7e
Gushchin Andrei <neogan74@gmail.com>
2023-09-28 09:20:38 +0400

    add logger

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,33 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 
-	"github.com/ardanlabs/conf"
-	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
+var service = "SALES-API"
 
 func main() {
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Printf("maxprocs: %w\n", err)
+
+	// Constructing logger
+	logconfig := zap.NewProductionConfig()
+	logconfig.OutputPaths = []string{"stdout"}
+	logconfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	logconfig.DisableStacktrace = true
+	logconfig.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := logconfig.Build()
+	if err != nil {
+		fmt.Println("Error constructing logger", err)
 		os.Exit(1)
 	}
-	//automaxprocs.New()
-	_ = conf.Field{}
 
-	g := runtime.GOMAXPROCS(1)
-	log.Printf("starting server LEO build [%s] CPU[%d]  ", build, g)
-	defer log.Println("Service ended")
+	defer log.Sync()
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+	if err := run(log.Sugar()); err != nil {
+		log.Sugar().Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
 
-	log.Println("Stopping service")
+	log.Info("We are starting....")
+
+	// if _, err := maxprocs.Set(); err != nil {
+	// 	fmt.Printf("maxprocs: %w\n", err)
+	// 	os.Exit(1)
+	// }
+	// //automaxprocs.New()
+	// _ = conf.Field{}
+	// g := runtime.GOMAXPROCS(1)
+	// log.Printf("starting server LEO build [%s] CPU[%d]  ", build, g)
+	// defer log.Println("Service ended")
+	// shutdown := make(chan os.Signal, 1)
+	// signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	// <-shutdown
+	// log.Println("Stopping service")
+}
+
+func run(log *zap.SugaredLogger) error {
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/neogan74/svc3
 
 go 1.21.0
 
-require (
-	github.com/ardanlabs/conf v1.5.0
-	go.uber.org/automaxprocs v1.5.3
-)
+require go.uber.org/zap v1.26.0
+
+require go.uber.org/multierr v1.11.0 // indirect
