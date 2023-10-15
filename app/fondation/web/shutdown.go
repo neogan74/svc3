1faabec6cce742d62e3b0d86b4c5878fd4ffc118
Gushchin Andrei <neogan74@gmail.com>
2023-10-15 11:42:01 +0400

    Declaring Custom Error Types

---
+++ app/fondation/web/shutdown.go
@@ -0,0 +1,24 @@
+package web
+
+import "errors"
+
+// shutdownError is a type used to help with the graceful termination of the service
+type shutdownError struct {
+	Message string
+}
+
+// NewShutdownError returns an error that causes the framework to signal a graceful shutdown
+func NewShutdownError(message string) error {
+	return &shutdownError{message}
+}
+
+// Error is the implementation of the error interface
+func (se *shutdownError) Error() string {
+	return se.Message
+}
+
+// IsShutodwn checks to see if the shutdown error is contained in the specified error value
+func isShutdown(err error) bool {
+	var se *shutdownError
+	return errors.As(err, &se)
+}
